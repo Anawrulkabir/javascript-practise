cf3e000753fde40a67e1d0f27dd15248312b886f
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2024-02-02 22:00:39 +0600

    free-drink.js file added

---
+++ js-conditional-tasks/free-drink.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let bill = 600
+
+let free_coke = true;
+if(bill >= 500){
+    console.log("You will get a free coke.");
+}else{
+    console.log("Drink price is 30TK only.");
+}
